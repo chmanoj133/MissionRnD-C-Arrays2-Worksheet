ce26631bbca959cb86cb14f65f0dc28627558334
chmanoj133 <chmanoj133@missionrnd.com>
2016-01-09 12:58:51 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -14,12 +14,52 @@ ERROR CASES: Return NULL for invalid inputs.
 NOTES:
 */
 
+#include <stdio.h>
+
+bool strcmp(char *str1, char *str2);
+int string_length(char *str);
+
 struct transaction {
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+int countGreaterNumbers(struct transaction *Arr, int len, char *date) 
+{
+	int i;
+
+	if (Arr == NULL || len == NULL || date == NULL)
+		return -1;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (strcmp(Arr[i].date, date))
+			break;
+	}
+
+	if (i == -1)
+		return NULL;
+
+	return len - i - 1;
+}
+
+bool strcmp(char *str1, char *str2)
+{
+	int i;
+	int len = string_length(str1);
+	if (len != string_length(str2))
+		return false;
+	for (i = 0; i < len; i++)
+	{
+		if (str1[i] != str2[i])
+			return false;
+	}
+	return true;
+}
+
+int string_length(char *str){
+	int i;
+	for (i = 0; str[i] != '\0'; i++);
+	return i;
 }
